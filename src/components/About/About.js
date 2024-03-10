e6adc7d3112292c9b5266524e6dca070e9333034
AS-DarkCode <136073293+AS-DarkCode@users.noreply.github.com>
2024-03-10 14:23:31 +0530

    Add files via upload

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,6 @@ import Particle from "../Particle";
 import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
 import laptopSvg from "../../Assets/about.svg";
 import Toolstack from "./Toolstack";
 
